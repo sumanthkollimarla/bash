ec0f3aa20a2161cbbf53c70ff7e3596abdd22783
sumanth <sumanthpower007@gmail.com>
2023-06-04 18:08:47 +0530

    push

--- robot/create-server.sh
+++ robot/create-server.sh
@@ -2,7 +2,7 @@
 
  COMPONENT=$1
  AMI_ID="$(aws ec2 describe-images --region us-east-1 --filters "Name=name,Values=DevOps-LabImage-CentOS7" | jq '.Images[].ImageId' | sed -e 's/"//g')"
-SGID= "$(aws ec2 describe-security-groups   --filters Name=group-name,Values=b51-allow-all | jq '.SecurityGroups[].GroupId' | sed -e 's/"//g')"
+SGID= "$(aws ec2 describe-security-groups   --filters Name=group-name,Values=all-add | jq '.SecurityGroups[].GroupId' | sed -e 's/"//g')"
 
 echo  "AMI ID Used to launch instance is : $AMI_ID"
 echo "SG ID Used to launch instance is : $SGID"
