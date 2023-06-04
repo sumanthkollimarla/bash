5ede4135b4c8a6551d3e17999505dad0522dede1
sumanth <sumanthpower007@gmail.com>
2023-06-04 17:52:13 +0530

    push

--- robot/create-server.sh
+++ robot/create-server.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+ COMPONENT=$1
  AMI_ID="$(aws ec2 describe-images --region us-east-1 --filters "Name=name,Values=DevOps-LabImage-CentOS7" | jq '.Images[].ImageId' | sed -e 's/"//g')"
 SGID= "$(aws ec2 describe-security-groups   --filters Name=group-name,Values=b51-allow-all | jq '.SecurityGroups[].GroupId' | sed -e 's/"//g')"
 
