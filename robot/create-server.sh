f643bd39dc918e4b62d29d442e7a77a94ace023a
sumanth <sumanthpower007@gmail.com>
2023-06-04 17:50:15 +0530

    push

--- robot/create-server.sh
+++ robot/create-server.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 
  AMI_ID="$(aws ec2 describe-images --region us-east-1 --filters "Name=name,Values=DevOps-LabImage-CentOS7" | jq '.Images[].ImageId' | sed -e 's/"//g')"
+SGID= "$(aws ec2 describe-security-groups   --filters Name=group-name,Values=b51-allow-all | jq '.SecurityGroups[].GroupId' | sed -e 's/"//g')"
 
-echo $AMI_ID
+echo  "AMI ID Used to launch instance is : $AMI_ID"
+echo "SG ID Used to launch instance is : $SGID"
+echo $COMPONENT
 
-aws ec2 run-instances --image-id $AMI_ID --instance-type t2.micro | jq
+aws ec2 run-instances --image-id $AMI_ID --instance-type t2.micro --security-group-ids $SGID --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=${COMPONENT}}]"
