664fa3f270a3641f41806ed13be3071f55498651
sumanth <sumanthpower007@gmail.com>
2023-06-04 20:03:40 +0530

    push

--- robot/create-server.sh
+++ robot/create-server.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
+if [ -z "$1" ]; then 
+    echo -e "\e[31m Component name is required \n example usage is: \n\t bash create-server.sh componentName \e[0m"   
+    exit 1 
+fi 
+
  COMPONENT=$1
  AMI_ID="$(aws ec2 describe-images --region us-east-1 --filters "Name=name,Values=DevOps-LabImage-CentOS7" | jq '.Images[].ImageId' | sed -e 's/"//g')"
 SGID= "$(aws ec2 describe-security-groups   --filters Name=group-name,Values=all-add | jq '.SecurityGroups[].GroupId' | sed -e 's/"//g')"
@@ -8,7 +13,5 @@ echo  "AMI ID Used to launch instance is : $AMI_ID"
 echo "SG ID Used to launch instance is : $SGID"
 echo $COMPONENT
 
-aws ec2 run-instances --image-id $AMI_ID --instance-type t2.micro --security-group-ids $SGID --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=${COMPONENT}}]"
+PRIVATE_IP=$(aws ec2 run-instances --image-id $AMI_ID --instance-type t3.micro --security-group-ids $SGID  --instance-market-options "MarketType=spot, SpotOptions={SpotInstanceType=persistent,InstanceInterruptionBehavior=stop}"  --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=${COMPONENT}-${ENV}}]"| jq '.Instances[].PrivateIpAddress' | sed -e 's/"//g')
 
-Error parsing parameter '--tag-specifications': Expected: ',', received: ']' for input:
-ResourceType=instance,Tags=[{Key=Name,Value=}]
